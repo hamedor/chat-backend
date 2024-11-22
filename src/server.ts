e41cef2e2d7abf640c09ca552740bb0bc6b691f9
dmitri <popov.d@flgchina.ru>
2024-11-22 14:52:48 +0500

    fix: env for port

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,10 @@ import http from "http"
 import authRoutes from "@src/routes/auth";
 import cors from 'cors';
 import websocket from "@src/routes/websocket";
+import dotenv from 'dotenv';
+import * as process from "node:process";
 
+dotenv.config();
 const app = express()
 
 app.use(cors())
@@ -17,7 +20,7 @@ const server = http.createServer(app)
 
 websocket(server);
 
-const PORT = 3000
+const PORT = process.env.APP_PORT;
 server.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 })
